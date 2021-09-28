0cb06b70ca15b4c63f518aa54d64946df0d082d3
Ricky <ejb0329@gmail.com>
2021-09-28 13:55:41 +0900

    make terraform file with module

---
+++ Infra/variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_name" {
+  description = "Name of VPC"
+  type        = string
+  default     = "lol-mbti-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_azs" {
+  description = "Availability zones for VPC"
+  type        = list(string)
+  default     = ["ap-northeast-2a", "ap-northeast-2b", "ap-northeast-2c"]
+}
+
+variable "vpc_private_subnets" {
+  description = "Private subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "vpc_public_subnets" {
+  description = "Public subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.11.0/24"]
+}
--- Infra/vpc.tf
+++ Infra/vpc.tf
@@ -1,94 +1,14 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  
-  tags = {
-    Name = "lol-mbti-vpc"
-  }
-}
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "2.21.0"
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.0.0/24"
+  name = var.vpc_name
+  cidr = var.vpc_cidr
 
-  availability_zone = "ap-northeast-2a"
+  azs = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets = var.vpc_public_subnets
 
-  tags = {
-    Name = "lol-mbti-public-subnet"
-  }
-}
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.10.0/24"
-
-  availability_zone = "ap-northeast-2a"
-
-  tags = {
-    Name = "lol-mbti-private-subnet"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "lol-mbti-igw"
-  }
-}
-
-resource "aws_eip" "nat_1" {
-  vpc = true
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = {
-    Name = "lol-mbti-eip"
-  }
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_1.id
-  subnet_id = aws_subnet.public_subnet.id
-
-  tags = {
-    Name = "lol-mbti-nat"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "lol-mbti-public-rt"
-  }
-}
-
-resource "aws_route_table_association" "rt_association_public" {
-  subnet_id = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat.id
-  }
-
-  tags = {
-    Name = "lol-mbti-private-rt"
-  }
-}
-
-resource "aws_route_table_association" "rt_association_private" {
-  subnet_id = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private.id
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 }
